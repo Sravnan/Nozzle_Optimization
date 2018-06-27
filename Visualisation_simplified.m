52f0bcc6fbefbd6ae9dfc3bec134d6c797a0edc2
Sigurd Ravnan <sravnan@gmail.com>
2018-06-27 14:43:04 +0200

    Changed the objective to ISP from Thrust

--- Visualisation_simplified.m
+++ Visualisation_simplified.m
@@ -11,7 +11,7 @@ clear all
 dx=0.005;
 
 rThroat=0.005:dx:0.1;
-epsilon=1:2:50;
+epsilon=1.1:2:50;
 
 
 % Matrix of output values for combinations of design variables rThroat and epsilon: 
@@ -23,7 +23,7 @@ for j=1:1:length(epsilon)
     designVec(1) = rThroat(i);
     designVec(2) = epsilon(j);
  	% Objective function
-    obj = thrust(designVec);
+    obj = isp(designVec);
     % Grid value of objective function:
     fobj(j,i) = obj; 
      
@@ -35,6 +35,7 @@ for j=1:1:length(epsilon)
     con3(j,i) = con(3);    % Scaled stress constraint
     con4(j,i) = con(4);    % Scaled temperature constraint
     con5(j,i) = con(5);    % Scaled mass constraint
+%     con6(j,i) = con(6);    % Scaled min pressure constraint
 
   end
   toc
@@ -42,7 +43,7 @@ end
 toc
 %%
 close all
-contour(rThroat, epsilon, fobj,'ShowText','on')
+contour(rThroat, epsilon, fobj,[362:10:412],'ShowText','on')
 xlabel('Throat radius rThroat (m)'), ylabel('Area ratio epsilon (-)'), ...
    title('Figure 1, Design space of simplified problem final project')
 hold on
@@ -60,6 +61,9 @@ contour(rThroat, epsilon, con4, [0.1 0.1],'g--')   % Infeasible region
 
 contour(rThroat, epsilon, con5, [0.0 0.0],'m') % Mass constraint
 contour(rThroat, epsilon, con5, [0.1 0.1],'m--')   % Infeasible region
+
+% contour(rThroat, epsilon, con6, [0.0 0.0],'g') % pressure constraint
+% contour(rThroat, epsilon, con6, [0.1 0.1],'g--')   % Infeasible region
 
 grid
 
