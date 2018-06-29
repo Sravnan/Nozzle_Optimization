c40cd50f6c0e6d5cd398d4bc9106c18e13c9bc07
Martin Henkel <martin.henkel6@gmail.com>
2018-06-29 10:07:15 +0200

    Added rough outline

--- ZFD.m
+++ ZFD.m
@@ -2,8 +2,11 @@ function [ xopt,fopt,iterations ] = ZFD( F,C )
 %Zoutendijk's feasible direction method for constrained problem
 %optimization
 
+%% Search along direction
 
 
+%% Determine new best direction
 
+%% Check KKT conditions
 end
 
